fb9c8b3eb9e7acc253889c0e03cf2ab27565d5ff
aschultz25 <aschultz25@avenues.org>
2025-01-22 10:22:51 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+// Information about the data:
+// https://github.com/fivethirtyeight/data/tree/master/us-weather-history
+
+const weatherURL = "https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/us-weather-history/KNYC.csv"
+
+// https://p5js.org/reference/p5/p5.Table/
+let weatherTable
+
+let currentRow = 0
+let currentColumn = 1
+
+function preload() {
+  weatherTable = loadTable(weatherURL, 'csv', 'header')
+}
+
+function setup() {
+  createCanvas(400, 400);
+  noLoop()
+  textAlign(CENTER,CENTER)
+}
+
+function draw() {
+  background(220);
+  // weatherTable.getRowCount()
+  
+  text(weatherTable.getNum(currentRow,currentColumn),width/2,height/2)
+}
+
+function keyPressed() {
+  if( keyCode === DOWN_ARROW ) {
+    currentRow++
+    
+    redraw()
+  }
+  if( keyCode === RIGHT_ARROW ) {
+    currentColumn++
+    if( currentColumn >= weatherTable.getColumnCount() ) {
+      currentColumn = 1
+    }
+    redraw()
+  }
+}
